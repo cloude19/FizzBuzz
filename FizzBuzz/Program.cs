0fe71af88cdd2432f6cbd2339011710323ba4df3
cloude19 <troycloude@gmail.com>
2020-04-21 16:46:57 -0400

    FizzBuzz game with arguments to changes remainder values and Max length of game.

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,7 +6,48 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Lets Play Fizz Buzz! \n");
+            FizzBuzz(100, new int[] {3,5},false);
+        }
+
+        /// <summary>
+        /// Start FizzBuzz game with the Max value determening the last value in the game.
+        /// Play: determines if the game is being played or if a list is just being generatd.
+        /// Default Starting value is 0
+        /// </summary>
+        /// <param name="Max"></param>
+        /// <param name="Play"></param>
+        static void FizzBuzz(int Max, int[] Multiples ,bool Play)
+        {
+            int remainderOne = Multiples[0];
+            int remainderTwo = Multiples[1];
+
+            if (Play == false)
+            {
+                for (int x = 1; x < Max + 1; x++)
+                {
+                    remainderOne = x % 3;
+                    remainderTwo = x % 5;
+
+                    if(remainderOne == 0 && remainderTwo == 0) //<- check this first
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else if(remainderOne == 0)
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+                    else if(remainderTwo == 0)
+                    {
+                        Console.WriteLine("Buzz");
+                    }
+                    else
+                    {
+                        Console.WriteLine(x);
+                    }
+                }
+            }
+
         }
     }
 }
